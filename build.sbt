ec78b36b12220909e740e6701f7e62d009fb17e7
counter2015 <voidcounter@gmail.com>
2024-03-18 11:52:55 +0800

    add fake dependencies which could let scala-steward detect.

--- build.sbt
+++ build.sbt
@@ -9,3 +9,6 @@ lazy val root = (project in file(".")).
     scriptedLaunchOpts ++= List("-Xms1024m", "-Xmx1024m", "-XX:ReservedCodeCacheSize=128m", "-XX:MaxPermSize=256m", "-Xss2m", "-Dfile.encoding=UTF-8"),
     resolvers += Resolver.url("typesafe", url("https://repo.typesafe.com/typesafe/ivy-releases/"))(Resolver.ivyStylePatterns)
   )
+
+import Dependencies._
+libraryDependencies ++= coreDependency
---
+++ project/Dependencies.scala
@@ -0,0 +1,22 @@
+import sbt._
+
+object Dependencies {
+
+  val tapirVersion = "1.7.3"
+
+  val tapir = Seq(
+    "com.softwaremill.sttp.tapir" %% "tapir-http4s-server-zio" % tapirVersion,
+    "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % tapirVersion,
+    "com.softwaremill.sttp.tapir" %% "tapir-redoc-bundle" % tapirVersion,
+    "org.http4s" %% "http4s-blaze-server" % "0.23.14"
+  )
+
+  val zioLoggingVersion = "2.1.12"
+  val log =  Seq(
+    "ch.qos.logback" % "logback-classic" % "1.4.11",
+    "dev.zio" %% "zio-logging" % zioLoggingVersion,
+    "dev.zio" %% "zio-logging-slf4j" % zioLoggingVersion,
+  )
+
+  val coreDependency = tapir ++ log
+}
