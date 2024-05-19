cf5edee54115b25d18eb820e3297d1c479ce3487
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-05-19 00:03:02 +0000

    Update zio-logging, zio-logging-slf4j to 2.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
     "org.http4s" %% "http4s-blaze-server" % "0.23.16"
   )
 
-  val zioLoggingVersion = "2.2.3"
+  val zioLoggingVersion = "2.2.4"
   val log =  Seq(
     "ch.qos.logback" % "logback-classic" % "1.5.6",
     "dev.zio" %% "zio-logging" % zioLoggingVersion,
