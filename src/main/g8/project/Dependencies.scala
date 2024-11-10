f98c3237510600cb2e8061e3ad989802b3d6dab3
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-11-10 00:04:45 +0000

    Update http4s-blaze-server to 0.23.17

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     "com.softwaremill.sttp.tapir" %% "tapir-http4s-server-zio" % tapirVersion,
     "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % tapirVersion,
     "com.softwaremill.sttp.tapir" %% "tapir-redoc-bundle" % tapirVersion,
-    "org.http4s" %% "http4s-blaze-server" % "0.23.16"
+    "org.http4s" %% "http4s-blaze-server" % "0.23.17"
   )
 
   val zioLoggingVersion = "2.3.2"
