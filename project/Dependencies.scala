4e5e93b7624f30338d6b1a926ac512951c32617a
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2024-11-17 00:04:32 +0000

    Update tapir-http4s-server-zio, ... to 1.11.9

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val tapirVersion = "1.11.8"
+  val tapirVersion = "1.11.9"
 
   val tapir = Seq(
     "com.softwaremill.sttp.tapir" %% "tapir-http4s-server-zio" % tapirVersion,
